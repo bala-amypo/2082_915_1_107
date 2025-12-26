9406a0fcc8d996945ce1d6bb95b3925f2b4bc127
bala-amypo <bala@amypo.in>
2025-12-26 08:39:35 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -49,4 +49,15 @@ public class JwtFilter extends OncePerRequestFilter {
         
         filterChain.doFilter(request, response);
     }
+    @Override
+protected boolean shouldNotFilter(HttpServletRequest request) {
+
+    String path = request.getServletPath();
+
+    return path.startsWith("/swagger-ui")
+            || path.startsWith("/v3/api-docs")
+            || path.equals("/swagger-ui.html")
+            || path.startsWith("/auth");
+}
+
 }
