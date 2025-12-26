5aecf631927c1880c0827ace88404e8d94161788
bala-amypo <bala@amypo.in>
2025-12-26 08:49:15 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,29 +1,28 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://9096.408procr.amypo.ai");
+        server.setDescription("Production Server");
+
         return new OpenAPI()
-            .info(new Info()
-                .title("Parcel Damage Claim API")
-                .version("1.0")
-                .description("API for managing parcel damage claims"))
-            .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-            .components(new Components()
-                .addSecuritySchemes("Bearer Authentication", 
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")));
+                .info(new Info()
+                        .title("Demo API")
+                        .version("1.0")
+                        .description("Demo Application APIs"))
+                .servers(List.of(server));
     }
 }
